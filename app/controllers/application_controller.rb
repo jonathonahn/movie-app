978230dab2ed1ea1f8168c133c412d9ea1605150
jonathon <ahnjon77@gmail.com>
2021-09-14 09:18:54 -0700

    adds authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,9 +16,17 @@ def current_user
     end
   end
 end
+
 def authenticate_user
   unless current_user
     render json: {}, status: :unauthorized
   end
 end
+
+def authenticate_admin
+  unless current_user && current_user.admin
+    render json: {}, status: :unauthorized
+  end
+end
+
 end
